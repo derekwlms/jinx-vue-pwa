310f495b60eea71e67a8b918986c42be2444974e
Derek Williams <derekwlms@gmail.com>
2018-12-03 22:43:36 -0500

    Fix routing and Firebase config problems

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
-import Game from '@/views/Game.vue';
+import Game from './views/Game.vue';
 
 Vue.use(Router);
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -6,12 +6,13 @@
     </p>
     <p>
       It's inspired by 
-      <a href="https://okgo.net/2013/05/09/say-the-same-thing/">OK Go</a>'s awesome 
-      <a href="http://www.shakingearthdigital.com/portfolio/say-the-same-thing/">Say the Same Thing</a>
-      mobile <a href="https://www.highroadtouring.com/check-out-ok-gos-new-free-game-app-say-the-same-thing/">app</a>.
+      <a href="https://okgo.net/2013/05/09/say-the-same-thing/" target="__blank">OK Go</a>'s awesome 
+      <a href="http://www.shakingearthdigital.com/portfolio/say-the-same-thing/" target="__blank">Say the Same Thing</a>
+      mobile <a href="https://www.highroadtouring.com/check-out-ok-gos-new-free-game-app-say-the-same-thing/" target="__blank">app</a>.
     </p>
     <p>
-      <a href="https://github.com/derekwlms/jinx-vue-pwa">GitHub</a>
+      <a href="https://github.com/derekwlms/jinx-vue-pwa" target="__blank">GitHub</a>
     </p>
+    <p>v0.0.3 - 3 December 2018</p>
   </div>
 </template>
